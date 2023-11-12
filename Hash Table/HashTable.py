df7d42764ab052cc0c954c31d8723c57c7abf0bc
bittarwork <81069087+bittarwork@users.noreply.github.com>
2023-11-12 07:11:41 +0200

    hash tabel with collision

--- Hash Table/HashTable.py
+++ Hash Table/HashTable.py
@@ -2,9 +2,9 @@
 class HashTable : 
     def __init__(self , size ) : 
         self.MAX = size
-        self.arr = [None for i in range (self.MAX)]
+        self.arr = [[] for i in range (self.MAX)]
     # hashing function by ASCI
-    def hasing (self , keys) : 
+    def hashing (self , keys) : 
         h= 0 
         for key in keys : 
             asc = ord(key)
@@ -12,24 +12,38 @@ class HashTable :
         return h % self.MAX
     # add to the hash tabel 
     def __setitem__(self , key , value) : 
-        hasehdKey = self.hasing(key)
-        self.arr[hasehdKey] = value
+        hasehdKey = self.hashing(key)
+        found  = False
+        for index , element in enumerate(self.arr[hasehdKey]) : 
+            if (len(element) == 2 and element[0] == key) : 
+                self.arr[hasehdKey][index] = (key, value)
+                found = True
+                break
+        if not found : 
+            self.arr[hasehdKey].append((key, value))
     # get a vallue by key 
     def __getitem__ (self , key ) : 
-        hashedKey = self.hasing(key)
-        if (self.arr[hashedKey]) : 
-            return self.arr[hashedKey]
+        hashed_key = self.hashing(key)
+        if self.arr[hashed_key]:
+            for element in self.arr[hashed_key]:
+                if element[0] == key:
+                    return element[1]
         else : 
-            return "key is wrong"
+            return "key :{0} is not found in hashed tabel".format(key)
+        
     # delet item by key
     def __delitem__ (self , key) : 
-        hashedKey = self.hasing(key)
-        self.arr[hashedKey] = None
+        hashed_key = self.hashing(key)
+        if self.arr[hashed_key]:
+            for index,  element in enumerate(self.arr[hashed_key]):
+                if element[0] == key:
+                    del self.arr[hashed_key][index]
+        else:
+            return "key :{0} is not found in hashed tabel".format(key)
+        
 
-t = HashTable(100)
-t["osama bittar"] = 140
-t["osama bitta"] = 150
-t["osama bitr"] = 160
-t["osamatar"] = 170
-t["o bittar"] = 180
+
+
+
+
 
